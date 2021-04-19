96353524f54ef28ae472f14d6f11914b3c3590cf
Your Name <you@example.com>
2021-04-19 19:03:46 -0300

    change pic name, add first model (may be very wrong)

---
+++ src/app/models/news.model.ts
@@ -0,0 +1,10 @@
+export class News {
+  title: string = "";
+  subtitle: string = "";
+  creationDate : number = Date.now();
+  lastUpdated : number = Date.now();
+  content: string = "";
+
+
+  constructor() {}
+}
