2fd919a3048401920eac1b10400fc0bce86f6db9
Your Name <you@example.com>
2021-04-21 13:47:58 -0300

    add 'create' view

--- src/app/news/news-routing.module.ts
+++ src/app/news/news-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewsCreateComponent } from './create/news-create.component';
 import { NewsComponent } from './news.component';
 
 const routes: Routes = [
   {path: '', component: NewsComponent},
+  {path: 'create', component: NewsCreateComponent},
 ];
 
 @NgModule({
--- src/app/news/news.module.ts
+++ src/app/news/news.module.ts
@@ -4,13 +4,15 @@ import { NewsService } from './services/news.service';
 import { NewsRoutingModule } from './news-routing.module';
 import { NewsSummaryComponent } from './components/news-summary/news-summary.component';
 import { NewsComponent } from './news.component';
+import { NewsCreateComponent } from './create/news-create.component';
 
 
 
 @NgModule({
   declarations: [
     NewsSummaryComponent,
-    NewsComponent
+    NewsComponent,
+    NewsCreateComponent
   ],
   providers: [
     NewsService
