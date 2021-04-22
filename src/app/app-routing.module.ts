f2f281db42e6ecb16e7048a17183251bdd03331c
Your Name <you@example.com>
2021-04-22 00:18:58 -0300

    create 'user' module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {path: "", redirectTo: "home", pathMatch: "full"},
   {path: "home", component: HomeComponent},
-  {path: "news", loadChildren: () => import('./news/news.module').then(p => p.NewsModule)}
+  {path: "news", loadChildren: () => import('./news/news.module').then(m => m.NewsModule)},
+  {path: "user", loadChildren: () => import('./user/user.module').then(m => m.UserModule)}
 ];
 
 @NgModule({
