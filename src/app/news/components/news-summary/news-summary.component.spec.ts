5420401bbc39c30caeba2e6587a626d08d326eac
Bruno Demantova <brunoeckdema@gmail.com>
2021-04-26 15:30:41 -0300

    fix tests :D

--- src/app/news/components/news-summary/news-summary.component.spec.ts
+++ src/app/news/components/news-summary/news-summary.component.spec.ts
@@ -1,16 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NewsService } from '../../services/news.service';
 
 import { NewsSummaryComponent } from './news-summary.component';
 
-describe('NewsComponent', () => {
+describe('NewsSummaryComponent', () => {
   let component: NewsSummaryComponent;
   let fixture: ComponentFixture<NewsSummaryComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NewsSummaryComponent ]
-    })
-    .compileComponents();
+      declarations: [NewsSummaryComponent],
+      imports: [HttpClientModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
