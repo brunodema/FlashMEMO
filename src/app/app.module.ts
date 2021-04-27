24e2fb323a65b433fddcdbedcfb0d5d8254666d9
Bruno Demantova <brunoeckdema@gmail.com>
2021-04-27 15:55:18 -0300

    update components to consume from ngx-b4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NewsModule } from './news/news.module';
 import { SharedModule } from './shared/shared.module';
+
+// for bootstrap animations
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// boostrap collapsable
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+// boostrap dropbdown
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { HeaderComponent } from './common/header/header.component';
 import { UserPreferencesDropdownComponent } from './common/header/user-preferences-dropdown/user-preferences-dropdown.component';
@@ -24,9 +31,13 @@ import { UserPreferencesDropdownComponent } from './common/header/user-preferenc
     AppRoutingModule,
     HttpClientModule,
     NewsModule,
-    SharedModule
+    SharedModule,
+    // boostrap collapsable
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    BsDropdownModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
