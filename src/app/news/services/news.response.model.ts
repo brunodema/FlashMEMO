7a77d46100ac51e8ad7a8ed3185c92b93a6b3228
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2021-07-03 21:22:12 -0300

    remove (now) unnecessary file

--- src/app/news/services/news.response.model.ts
+++
@@ -1,8 +0,0 @@
-import { News } from '../models/news.model';
-
-export interface NewsListResponseModel {
-  errors: string[];
-  message: string;
-  news: News[]; // name must match with the returned object (lesson learned)
-  status: string;
-}
