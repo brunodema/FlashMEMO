ab5e5e407c2644bf2f62681ef7bba3a359e42037
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2021-09-24 23:46:13 -0300

    set data for k8s deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendRootAddress: 'https://flashmemo-backend:443',
+  backendRootAddress: 'https://flashmemo.edu:8081',
   maxPageSize: 10000,
 };
