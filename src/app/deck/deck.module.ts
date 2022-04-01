1316ad82ed2cb0d041154d75c2faae1f461a9e53
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-04-01 00:57:42 -0300

    change routing declarations to conform to angular examples

--- src/app/deck/deck.module.ts
+++ src/app/deck/deck.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DeckListComponent } from './deck-list/deck-list.component';
 import { DeckDetailComponent } from './deck-detail/deck-detail.component';
+import { DeckRoutingModule } from './deck-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [DeckListComponent, DeckDetailComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, DeckRoutingModule, SharedModule],
 })
 export class DeckModule {}
