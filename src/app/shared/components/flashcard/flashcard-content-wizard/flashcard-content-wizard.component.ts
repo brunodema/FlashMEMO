aad3cc34efd2540b94fdfb8837a97557fffdc2f4
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-04-05 02:11:44 -0300

    add prototype layout selection stuff

--- src/app/shared/components/flashcard/flashcard-content-wizard/flashcard-content-wizard.component.ts
+++ src/app/shared/components/flashcard/flashcard-content-wizard/flashcard-content-wizard.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+/* TIL about the 'host'property
+
+Apparently we can use this property to set things related to the wrapper element that Angular will add to the DOM after compiling the app. 
+In this case, I add all those classes so the layout of the rows/columns is the way I want. */
+
 @Component({
   selector: 'app-flashcard-content-wizard',
+  host: { class: 'flex-fill d-flex align-items-center justify-content-center' },
   templateUrl: './flashcard-content-wizard.component.html',
-  styleUrls: ['./flashcard-content-wizard.component.css']
+  styleUrls: ['./flashcard-content-wizard.component.css'],
 })
 export class FlashcardContentWizardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
