dc01a7eedf88ea564792e8fdad0bd98c22f034b0
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-04-07 22:10:56 -0300

    remove dead code

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,12 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { PaginatedListResponse } from 'src/app/shared/models/api-response';
-import { SortOptions } from 'src/app/shared/models/sort-classes/sort-options';
-import { SortType } from 'src/app/shared/models/sort-classes/sort-type';
 import { environment } from 'src/environments/environment';
-import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root',
@@ -15,15 +9,4 @@ export class UserService {
   protected testServiceURL: string = `${environment.backendRootAddress}/api/v1/User/list`;
 
   constructor(private http: HttpClient) {}
-
-  getAllUsers(sortType: SortType = SortType.NONE): Observable<User[]> {
-    let pageSize = environment.maxPageSize;
-    let comparisonFunction = SortOptions.getComparisonFunction(sortType);
-
-    return this.http
-      .get<PaginatedListResponse<User>>(
-        `${this.testServiceURL}?pageSize=${pageSize}`
-      )
-      .pipe(map((a) => a.data.results));
-  }
 }
