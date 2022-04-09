30fd3b0af30d03b4b6106c0b39b337ccced0211c
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-04-09 20:27:02 -0300

    update implementation of UserService

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,12 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IServiceSearchParams } from 'src/app/shared/models/other/api-query-types';
+import { GeneralRepositoryService } from 'src/app/shared/services/data-table-service';
 import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserService {
-  protected testServiceURL: string = `${environment.backendRootAddress}/api/v1/User/list`;
+export class UserService extends GeneralRepositoryService<User> {
+  constructor(private http: HttpClient) {
+    super(`${environment.backendRootAddress}/api/v1/User`, http);
+  }
 
-  constructor(private http: HttpClient) {}
+  search(searchParams: IServiceSearchParams): Observable<User[]> {
+    throw new Error('Method not implemented.');
+  }
 }
