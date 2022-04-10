0d6cafad28ba0cd5c40cc781e4941a9922b772a9
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-04-10 16:53:30 -0300

    finish implementing mock, add extra paginated list classes

--- src/app/shared/models/http/response-interfaces.ts
+++ src/app/shared/models/http/response-interfaces.ts
@@ -18,9 +18,51 @@ export interface IPaginatedList<Type> {
   hasNextPage: boolean;
 }
 
+/**
+ * Similar to the common implementaiton, but Uses string properties to hold values, in case they are too large in terms of bits.
+ */
+export interface ILargePaginatedList<Type> {
+  results: Type[];
+  pageIndex: string;
+  totalPages: string;
+  resultSize: string; // different naming than the other class, VERY DANGEROUS
+  totalAmount: string; // different naming than the other class, VERY DANGEROUS
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
+}
+
+/**
+ * I have to implement this class so I can declare objects of this type when using implementation outside Angular's libraries.
+ */
+export class LargePaginatedList<Type> implements ILargePaginatedList<Type> {
+  results: Type[];
+  pageIndex: string;
+  totalPages: string;
+  resultSize: string; // different naming than the other class, VERY DANGEROUS
+  totalAmount: string; // different naming than the other class, VERY DANGEROUS
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
+}
+
 export interface IPaginatedListResponse<Type> {
   errors: string[];
   message: string;
-  data: IPaginatedList<Type>; // name must match with the returned object (lesson learned)
+  data: IPaginatedList<Type> | ILargePaginatedList<Type>; // property name must match with the returned object (lesson learned)
+  status: string;
+}
+
+/**
+ * I have to implement this class so I can declare objects of this type when using implementation outside Angular's libraries.
+ */
+export class PaginatedListResponse<Type>
+  implements IPaginatedListResponse<Type>
+{
+  public constructor(init?: Partial<IPaginatedListResponse<Type>>) {
+    Object.assign(this, init);
+  }
+
+  errors: string[];
+  message: string;
+  data: IPaginatedList<Type> | ILargePaginatedList<Type>;
   status: string;
 }
--- src/app/shared/services/api-services.ts
+++ src/app/shared/services/api-services.ts
@@ -1,5 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {
+  PaginatedListResponse,
+  IPaginatedListResponse,
+} from '../models/http/response-interfaces';
+
+/**
+ * Dummy class for testing purposes.
+ */
+export interface IImageAPIResult {}
+
+export class GoogleImageResult implements IImageAPIResult {
+  title: string;
+  link: string;
+  image = new (class {
+    byteSize: number;
+    contextLink: string;
+    height: number;
+    thumbnailHeight: number;
+    thumbnailLink: string;
+    thumbnailWidth: number;
+    width: number;
+  })();
+}
 
 @Injectable()
 export abstract class GeneralImageAPIService {
@@ -8,11 +31,62 @@ export abstract class GeneralImageAPIService {
     protected httpClient: HttpClient
   ) {}
 
-  abstract searchImage(keyword: String): String;
+  /**
+   * Searches for images related to the provided keyword. Returns a IPaginatedListResponse<Type> response, containing information on the request itself, and of the returned data (page size/index, results themselves, etc).
+   * @param keyword Word to be used for the search.
+   */
+  abstract searchImage(
+    keyword: String
+  ): IPaginatedListResponse<IImageAPIResult>;
 }
 
 export class MockImageAPIService extends GeneralImageAPIService {
-  searchImage(keyword: String): String {
-    return 'https://www.freecodecamp.org/portuguese/news/content/images/2022/03/Untitled-design--1-.png'; // returns some random wachy image
+  searchImage(keyword: String): PaginatedListResponse<IImageAPIResult> {
+    return new PaginatedListResponse<GoogleImageResult>({
+      message: 'Success',
+      status: '200',
+      data: {
+        hasNextPage: false,
+        hasPreviousPage: false,
+        pageIndex: '1',
+        totalAmount: '2',
+        resultSize: '2',
+        totalPages: '1',
+        results: [
+          {
+            title:
+              'L.O.L. Surprise! L.O.L. Surprise OMG Sports Doll- Cheer 577508 ...',
+            image: {
+              byteSize: 807147,
+              contextLink:
+                'https://www.bestbuy.com/site/l-o-l-surprise-l-o-l-surprise-omg-sports-doll-cheer/6463576.p?skuId=6463576',
+              height: 4409,
+              thumbnailHeight: 150,
+              thumbnailLink:
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcdFWm87QgFJzd0aqHu9r1Lg01S7ca7-_0ijLbfm7ddj-eVcpd1BfbHWU&s',
+              thumbnailWidth: 112,
+              width: 3306,
+            },
+            link: 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6463/6463576_sd.jpg',
+          },
+          {
+            title:
+              'How L.O.L. Dolls Became the Dopamine Hit of a Generation - The New ...',
+            image: {
+              byteSize: 553409,
+              contextLink:
+                'https://www.nytimes.com/2020/04/16/parenting/lol-surprise-doll-isaac-larian.html',
+              height: 1200,
+              thumbnailHeight: 100,
+              thumbnailLink:
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8OfjT1pLQpy-wkPfTn0vKyTHcVMR005he34rKpZt0XKQJ3VOa88Tc3wk&s',
+              thumbnailWidth: 150,
+              width: 1800,
+            },
+            link: 'https://static01.nyt.com/images/2019/12/17/multimedia/17-parenting-LOL1/merlin_165750795_642b4b50-77fd-4ba8-8a78-1ba763e3cd7a-mobileMasterAt3x.jpg',
+          },
+        ],
+      },
+    });
   }
 }
