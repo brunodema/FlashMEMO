dccda05ffa71cff5cd0bf8f1c94ed2c8898c956a
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-04-11 23:44:11 -0300

    refactor a bit of layout component

--- src/app/shared/components/flashcard/flashcard-layout/flashcard-layout.component.ts
+++ src/app/shared/components/flashcard/flashcard-layout/flashcard-layout.component.ts
@@ -4,8 +4,7 @@ import { FlashcardLayout } from 'src/app/shared/models/flashcard';
 @Component({
   selector: 'app-flashcard-layout',
   host: {
-    class:
-      'd-flex flex-column flex-fill align-items-center justify-content-center',
+    class: 'h-100',
   },
   templateUrl: './flashcard-layout.component.html',
   styleUrls: ['./flashcard-layout.component.css'],
