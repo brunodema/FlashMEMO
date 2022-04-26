5c3ee66b5a002d47422ca4a93553a0a696ed8307
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-04-26 18:54:19 -0300

    trying to fix DI in DataTableComponent

--- src/app/user/components/user-list/user-list.component.ts
+++ src/app/user/components/user-list/user-list.component.ts
@@ -4,25 +4,29 @@ import { NewsService } from 'src/app/news/services/news.service';
 import { GeneralRepositoryService } from 'src/app/shared/services/general-repository-service';
 import { RouteMap } from 'src/app/shared/models/routing/route-map';
 import { DataTableComponent } from 'src/app/shared/components/data-table/data-table.component';
+import { Deck } from 'src/app/deck/models/deck.model';
+import { Flashcard } from 'src/app/shared/models/flashcard';
+import {
+  DeckService,
+  MockDeckService,
+} from 'src/app/deck/services/deck.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
-  providers: [{ provide: GeneralRepositoryService, useClass: NewsService }],
+  providers: [{ provide: GeneralRepositoryService, useClass: MockDeckService }],
 })
 export class UserListComponent implements AfterViewInit {
-  displayedColumns: string[] = ['newsID', 'title', 'content'];
-  pageSizeOptions: number[] = [5, 10, 25];
-
   routes: RouteMap[] = [{ label: 'Create User', route: 'create' }];
 
-  @ViewChild(DataTableComponent) dataTable: DataTableComponent<News>;
+  @ViewChild('lol') dataTable: DataTableComponent<Deck>;
+  @ViewChild('lolo') dataTable2: DataTableComponent<Flashcard>;
 
   constructor(public service: GeneralRepositoryService<News>) {}
 
   ngAfterViewInit(): void {
-    this.dataTable.displayedColumns = this.displayedColumns;
-    this.dataTable.pageSizeOptions = this.pageSizeOptions;
+    console.log(this.dataTable);
+    console.log(this.dataTable2);
   }
 }
