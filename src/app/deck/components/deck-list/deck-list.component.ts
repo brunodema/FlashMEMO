cc18f43dce4c2ef2e44ad139df8d9c642876254e
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-04-28 17:30:46 -0300

    toy around with event handlers

--- src/app/deck/components/deck-list/deck-list.component.ts
+++ src/app/deck/components/deck-list/deck-list.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import {
   DataTableColumnOptions,
   DataTableComponent,
+  DataTableComponentClickEventArgs,
 } from 'src/app/shared/components/data-table/data-table.component';
 import { RouteMap } from 'src/app/shared/models/routing/route-map';
 import { Deck } from '../../models/deck.model';
@@ -29,5 +30,10 @@ export class DeckListComponent {
 
   constructor(public service: GenericDeckService) {
     this.service.getAll().subscribe((x) => (this.deckData = x));
+  }
+
+  handleDeleteDeck(args: DataTableComponentClickEventArgs<Deck>) {
+    if (confirm(`Are you sure you want to delete deck '${args.rowData.name}'?`))
+      console.log('deck deleted! (but actually not)');
   }
 }
