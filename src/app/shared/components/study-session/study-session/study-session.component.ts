9addec26d97db14f948eaff2c59567cc06f12d85
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-05-17 21:01:55 -0300

    WIP (study session)

--- src/app/shared/components/study-session/study-session/study-session.component.ts
+++ src/app/shared/components/study-session/study-session/study-session.component.ts
@@ -6,16 +6,37 @@ export enum StudySessionStep {
   END,
 }
 
+export class StudySessionImageTools {
+  studySessionImages = [
+    'assets\\study_session\\study-session-1.jpg',
+    'assets\\study_session\\study-session-2.jpg',
+    'assets\\study_session\\study-session-3.jpg',
+  ];
+
+  pickRandomImage(): string {
+    return this.studySessionImages[
+      Math.floor(Math.random() * this.studySessionImages.length)
+    ];
+  }
+}
+
 @Component({
   selector: 'app-study-session',
   templateUrl: './study-session.component.html',
   styleUrls: ['./study-session.component.css'],
+  host: { class: 'd-flex flex-column h-100' },
 })
 export class StudySessionComponent implements OnInit {
   StudySessionStepEnum = StudySessionStep;
   currentStep: StudySessionStep = StudySessionStep.START;
 
-  constructor() {}
+  startImg: string;
+  endImg: string;
+
+  constructor() {
+    this.startImg = new StudySessionImageTools().pickRandomImage();
+    this.endImg = new StudySessionImageTools().pickRandomImage();
+  }
 
   ngOnInit(): void {}
 }
