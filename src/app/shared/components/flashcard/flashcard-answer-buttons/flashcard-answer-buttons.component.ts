722c67d5b5e43584006921bce627c18e646dc0c1
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-05-18 21:34:36 -0300

    remove unnecessary interface

--- src/app/shared/components/flashcard/flashcard-answer-buttons/flashcard-answer-buttons.component.ts
+++ src/app/shared/components/flashcard/flashcard-answer-buttons/flashcard-answer-buttons.component.ts
@@ -10,14 +10,14 @@ export enum FlashcardReviewStatus {
   selector: 'app-flashcard-answer-buttons',
   templateUrl: './flashcard-answer-buttons.component.html',
   styleUrls: ['./flashcard-answer-buttons.component.css'],
-  host: { class: 'd-flex flex-row w-100 h-100 justify-content-around' },
+  host: {
+    class: 'd-flex flex-row w-100 h-100 justify-content-around',
+  },
 })
-export class FlashcardAnswerButtonsComponent implements OnInit {
+export class FlashcardAnswerButtonsComponent {
   @Output()
   answerClick: EventEmitter<FlashcardReviewStatus> = new EventEmitter();
   flashcardReviewStatusEnum = FlashcardReviewStatus;
 
   constructor() {}
-
-  ngOnInit(): void {}
 }
