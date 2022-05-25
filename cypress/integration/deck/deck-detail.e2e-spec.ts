d49e71b1a20d0bc963b5205f78d5f117aaf0eaa6
Bruno Demantova <64618343+brunodema@users.noreply.github.com>
2022-05-25 03:11:19 +0000

    improve deck-detail testing

--- cypress/integration/deck/deck-detail.e2e-spec.ts
+++ cypress/integration/deck/deck-detail.e2e-spec.ts
@@ -1,33 +1,43 @@
 // used this to setup tests: https://testing-angular.com/introduction/#introduction
+// shout-outs to protractor, that got deprecated by Angular itself, and cost 1-2 days of my life
 
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/');
-    cy.contains('FlashMEMO');
+import deckJson from 'src/assets/test_assets/Decks.json';
 
-    cy.url().then((url) =>
-      cy.task('log', url)
-    );
+class DeckDetailPageObject {
+  visitDeckDetail(id?: string) {
+    if (id) cy.visit(`deck/${id}`);
+    else cy.visit('deck/create');
+  }
+
+  getSaveDeckButton() {
+    cy.get('data-testid="save-deck-btn"');
+  }
+
+  getStudySessionButton() {
+    cy.get('data-testid="study-session-btn"');
+  }
+
+  logCurrentURL() {
+    cy.url().then((url) => cy.task('log', url));
+  }
+}
+
+describe('Access deck-detail and find stuff', () => {
+  let page: DeckDetailPageObject = new DeckDetailPageObject();
+
+  it('Goes to the fucking page (via "create")', () => {
+    page.visitDeckDetail();
   });
-});
 
-describe('Will we finally access deck-detail?', () => {
-  it('Goes to the fucking page', () => {
-    cy.visit('deck/create');
-    cy.url().then((url) =>
-      cy.task('log', url)
-    );
-
-    cy.get('[data-testid="study-session-btn"]');
+  it('Goes to the fucking page (via "deckId")', () => {
+    page.visitDeckDetail(deckJson[0].deckId);
   });
 
-  it('There is no study button on this page', () => {
-    cy.visit('deck/list');
-    cy.url().then((url) =>
-      cy.task('log', url)
-    );
+  it('Goes to the fucking page (with wrong id?)', () => {
+    page.visitDeckDetail('shenanigans');
+    cy.url().should('contain', 'home');
+    cy.url().should('not.contain', 'deck');
 
-    cy.get('[data-testid="study-session-btn"]').should('not.exist');
-    // cy.get('[data-testid="study-session-btn"]').should('exist')
+    page.logCurrentURL()
   });
 });
