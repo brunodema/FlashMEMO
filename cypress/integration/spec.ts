1bd0574cc67a800d46bd6148e4df1e3e1e44d1f0
Bruno Demantova <64618343+brunodema@users.noreply.github.com>
2022-05-25 00:48:22 +0000

    AN E2E FRAMEWORK THAT ACTUALLY WORKS!

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,4 +1,4 @@
-import cypress from 'cypress';
+// used this to setup tests: https://testing-angular.com/introduction/#introduction
 
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
@@ -8,5 +8,26 @@ describe('My First Test', () => {
     cy.url().then((url) =>
       cy.task('log', url)
     );
+  });
+});
+
+describe('Will we finally access deck-detail?', () => {
+  it('Goes to the fucking page', () => {
+    cy.visit('deck/create');
+    cy.url().then((url) =>
+      cy.task('log', url)
+    );
+
+    cy.get('[data-testid="study-session-btn"]');
+  });
+
+  it('There is no study button on this page', () => {
+    cy.visit('deck/list');
+    cy.url().then((url) =>
+      cy.task('log', url)
+    );
+
+    cy.get('[data-testid="study-session-btn"]').should('not.exist');
+    // cy.get('[data-testid="study-session-btn"]').should('exist')
   });
 });
