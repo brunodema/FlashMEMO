4301b13a7f8120d6aef8ddd109a273a688e97daf
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-06-05 19:03:52 -0300

    add thumbnail button for 'create' view

--- src/app/news/components/common/news-form/news-form.component.ts
+++ src/app/news/components/common/news-form/news-form.component.ts
@@ -1,22 +1,34 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { CKEditor4 } from 'ckeditor4-angular';
 import { News } from 'src/app/news/models/news.model';
+import { GenericNotificationService } from 'src/app/shared/services/notification/notification.service';
 
 @Component({
   selector: 'app-news-form',
   templateUrl: './news-form.component.html',
-  styleUrls: ['./news-form.component.css']
+  styleUrls: ['./news-form.component.css'],
 })
 export class NewsFormComponent {
-
   /**
    * Model used to setup the new News object.
    */
   @Input()
   model: News = new News();
 
+  /**
+   * Emit event stating the the news model from the form is ready for further processing (ex: CRUD).
+   */
   @Output()
-  submit : EventEmitter<News> = new EventEmitter()
+  submit: EventEmitter<News> = new EventEmitter();
 
   editorType: CKEditor4.EditorType = CKEditor4.EditorType.CLASSIC;
   editorConfig: CKEditor4.Config = {
@@ -40,10 +52,42 @@ export class NewsFormComponent {
     ],
   };
 
-  constructor() { }
+  /**
+   * Determines if the 'preview' section is shown to the user or not.
+   */
+  public showImagePreview: boolean = false;
+
+  /**
+   * Handle to access the image preview widget on the form.
+   */
+  @ViewChild('imagePreview') imagePreview: ElementRef<any>;
+
+  constructor(
+    protected notificationService: GenericNotificationService,
+    protected crd: ChangeDetectorRef
+  ) {
+    this.showImagePreview = this.model.thumbnailPath.trim().length > 0;
+  }
 
   onSubmit() {
-    console.log(this.model);
-    this.submit.emit(this.model)
+    this.submit.emit(this.model);
+  }
+
+  processThumbnailURL() {
+    this.showImagePreview = true;
+    this.crd.detectChanges();
+    this.imagePreview.nativeElement.src = this.model.thumbnailPath;
+  }
+
+  brokenImage() {
+    this.showImagePreview = false;
+    this.notificationService.showError(
+      'The provided URL does not correspond to a valid image!'
+    );
+  }
+
+  clearImagePreview() {
+    this.showImagePreview = false;
+    this.model.thumbnailPath = '';
   }
 }
