8849cb4f071500de2b32c125fd8d6580c48c785e
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-06-06 23:46:18 -0300

    apply "fix" for CKEditor inside MatTabGroup

--- src/app/news/components/news-detail/news-detail.component.ts
+++ src/app/news/components/news-detail/news-detail.component.ts
@@ -28,11 +28,6 @@ export class NewsDetailComponent
    */
   @ViewChild('tabGroup', { static: false }) tabGroup: MatTabGroup;
 
-  /**
-   * Determines if the view will show the preview for an existing news upon opening or not.
-   */
-  protected isNewNews: boolean = true;
-
   constructor(
     @Inject('GenericNewsService')
     protected service: GenericNewsService,
@@ -44,12 +39,17 @@ export class NewsDetailComponent
 
     if (!this.model) {
       this.model = new News();
-    } else {
-      this.isNewNews = false;
     }
   }
   ngAfterViewInit(): void {
     // CKEditor is bugged when it comes to having an editor inside a MatTabGroup. If I set the other tab as default (form hidden at first), an error will be thrown and no content will be set to the editor. Even though it's stated that a fix for it exists, it doesn't work on multiple reloads (ex: browsing news 'list' view and selecting different News there). The issue is mentioned on many places, like here: https://github.com/ckeditor/ckeditor4-angular/issues/114. For the first time, I couldn't find an actual solution for this - maybe ditching the MatTabGroup? For now, I'll leave the form view as the default, since no problems occur when set like this. I nthe future, I'll probably will still need a 'detail' view for News so those tabs aren't show for a normal user.
+
+    // UPDATE: Using the lazy loading described in the Angular Material documentation (https://material.angular.io/components/tabs/overview#lazy-loading) fixes the CKEditor content problems, BUT keeps the exception on the console. I guess I'll have to live with this error being thrown...
+    if (this.model.newsId.length > 0) {
+      this.tabGroup.selectedIndex = 1;
+    } else {
+      this.tabGroup.selectedIndex = 0;
+    }
   }
 
   onSubmit(args: News) {
