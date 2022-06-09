3a2145c434e0135eec32687501e4fca45fac9f5d
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-06-09 17:45:15 -0300

    add owner info for news-summary component

--- src/app/news/components/common/news-summary/news-summary.component.ts
+++ src/app/news/components/common/news-summary/news-summary.component.ts
@@ -1,4 +1,6 @@
 import { Component, Inject, OnInit, Pipe } from '@angular/core';
+import { User } from 'src/app/user/models/user.model';
+import { GenericUserService } from 'src/app/user/services/user.service';
 import { News } from '../../../models/news.model';
 import {
   GenericNewsService,
@@ -11,14 +13,21 @@ import {
   styleUrls: ['./news-summary.component.css'],
 })
 export class NewsSummaryComponent implements OnInit {
-  public newsList: News[];
+  public newsList: News[] = [];
+  public ownerInfo: User[] = [];
 
   constructor(
-    @Inject('GenericNewsService') private newsService: GenericNewsService
+    @Inject('GenericNewsService') private newsService: GenericNewsService,
+    @Inject('GenericUserService') private userService: GenericUserService
   ) {}
 
   ngOnInit() {
     this.newsService.getLatestNews(4).subscribe((latestNews) => {
+      latestNews.forEach((news) => {
+        this.userService.get(news.ownerId).subscribe((getResponse) => {
+          this.ownerInfo.push(getResponse.data); // no need to reset array here because it is not updated automatically like in other components
+        });
+      });
       this.newsList = latestNews;
     });
   }
