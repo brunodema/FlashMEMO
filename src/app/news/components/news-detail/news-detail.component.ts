7065bd037cd7493f929d7f8144eb8eb364b8e531
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-06-11 13:27:10 -0300

    get ownerId from authService

--- src/app/news/components/news-detail/news-detail.component.ts
+++ src/app/news/components/news-detail/news-detail.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, Inject, ViewChild } from '@angular/core';
 import { MatTabGroup } from '@angular/material/tabs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DetailViewComponent } from 'src/app/shared/components/common/detail-view/detail-view.component';
+import { GenericAuthService } from 'src/app/shared/services/auth.service';
 import { GenericNotificationService } from 'src/app/shared/services/notification/notification.service';
 import { News } from '../../models/news.model';
 import { GenericNewsService } from '../../services/news.service';
@@ -23,6 +24,8 @@ export class NewsDetailComponent
   constructor(
     @Inject('GenericNewsService')
     protected newsService: GenericNewsService,
+    @Inject('GenericAuthService')
+    protected authService: GenericAuthService,
     protected notificationService: GenericNotificationService,
     protected router: Router,
     protected route: ActivatedRoute
@@ -32,6 +35,8 @@ export class NewsDetailComponent
     if (!this.model) {
       this.model = new News();
     }
+
+    this.model.ownerId = this.authService.loggedUserId.getValue();
   }
   ngAfterViewInit(): void {
     // CKEditor is bugged when it comes to having an editor inside a MatTabGroup. If I set the other tab as default (form hidden at first), an error will be thrown and no content will be set to the editor. Even though it's stated that a fix for it exists, it doesn't work on multiple reloads (ex: browsing news 'list' view and selecting different News there). The issue is mentioned on many places, like here: https://github.com/ckeditor/ckeditor4-angular/issues/114. For the first time, I couldn't find an actual solution for this - maybe ditching the MatTabGroup? For now, I'll leave the form view as the default, since no problems occur when set like this. I nthe future, I'll probably will still need a 'detail' view for News so those tabs aren't show for a normal user.
