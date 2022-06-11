406dcf96796dd93a48a4fc9ee4e49b6a9999556d
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-06-11 13:12:29 -0300

    add new local testing env

---
+++ src/environments/environment.local-testing.ts
@@ -0,0 +1,26 @@
+import { DeckService } from 'src/app/deck/services/deck.service';
+import { NewsService } from 'src/app/news/services/news.service';
+import { AudioService } from 'src/app/shared/services/APIs/audio-api.service';
+import { DictionaryService } from 'src/app/shared/services/APIs/dictionary-api.service';
+import { ImageAPIService } from 'src/app/shared/services/APIs/image-api.service';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { FlashcardService } from 'src/app/shared/services/flashcard.service';
+import { LanguageService } from 'src/app/shared/services/language.service';
+import { UserService } from 'src/app/user/services/user.service';
+
+export const environment = {
+  production: false,
+  backendRootAddress: 'http://api.flashmemo.edu:5000',
+  maxPageSize: 10000,
+  defaultLanguageISOCode: 'en-gb',
+  // services
+  newsService: NewsService,
+  userService: UserService,
+  deckService: DeckService,
+  flashcardService: FlashcardService,
+  authService: AuthService,
+  languageService: LanguageService,
+  imageAPIService: ImageAPIService,
+  audioAPIService: AudioService,
+  dictionaryAPIService: DictionaryService,
+};
