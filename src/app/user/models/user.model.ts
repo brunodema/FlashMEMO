bf3737ffe59e4c927ed07d3a7f34b3a88c1b3941
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-06-16 16:06:41 -0300

    add initialization to properties (just in case)

--- src/app/user/models/user.model.ts
+++ src/app/user/models/user.model.ts
@@ -1,9 +1,11 @@
+import { Guid } from 'guid-ts';
+
 export class User {
-  id: string;
-  name: string;
-  surname: string;
-  username: string;
-  email: string;
+  id: string = Guid.newGuid().toString();
+  name: string = '';
+  surname: string = '';
+  username: string = '';
+  email: string = '';
 
   public constructor(init?: Partial<User>) {
     Object.assign(this, init);
