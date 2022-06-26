451f21e625317061faaa9b32f48fea28bb7865db
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-06-26 19:56:03 -0300

    checkpoint before updating npm packages

--- src/app/root/components/home/home.component.ts
+++ src/app/root/components/home/home.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { GenericLoggerService } from 'src/app/shared/services/logging/logger.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(
+    @Inject('GenericLoggerService')
+    protected loggerService: GenericLoggerService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
