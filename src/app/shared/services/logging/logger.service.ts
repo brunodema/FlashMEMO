9ee6c15f21d7a2c7b378ceca8bfdfa0d568d1d84
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-06-26 19:37:02 -0300

    add more stuff to logging classes

--- src/app/shared/services/logging/logger.service.ts
+++ src/app/shared/services/logging/logger.service.ts
@@ -1,13 +1,50 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Logger } from 'tslog';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class LoggerService {
+export interface IFlashMEMOLoggerOptions {
+  logLevel: 'DEBUG' | 'TRACE' | 'WARNING' | 'INFORMATION' | 'ERROR';
+  provider: 'CONSOLE' | 'API';
+}
+
+export interface IFlashMEMOLogger {
+  logDebug(): void;
+  logTrace(): void;
+  logError(): void;
+  logInformation(): void;
+  logWarning(): void;
+}
+
+export abstract class GenericLoggerService implements IFlashMEMOLogger {
   protected loggerInstance: Logger;
 
   constructor() {
     this.loggerInstance = new Logger();
+  }
+
+  logInformation(): void {
+    this.loggerInstance.info();
+  }
+  logDebug(): void {
+    this.loggerInstance.debug();
+  }
+  logTrace(): void {
+    this.loggerInstance.trace();
+  }
+  logError(): void {
+    this.loggerInstance.error();
+  }
+  logWarning(): void {
+    this.loggerInstance.warn();
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoggerService extends GenericLoggerService {
+  constructor(
+    @Inject('LOGGER_OPTIONS') protected options: IFlashMEMOLoggerOptions
+  ) {
+    super();
   }
 }
